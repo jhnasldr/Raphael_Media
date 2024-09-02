58ad37eefd41ed2a6de48c6d363b371b11f0155d
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2024-09-02 08:26:55 +0200

    Alex, Skapa en data.sql + la in 3st video Object i databasen

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO video (title, url, release_date) VALUES
+                                                 ('Introduction to Java', 'https://rapharlstudio/video/java-intro', '2023-01-10'),
+                                                 ('Advanced Spring Boot', 'https://rapharlstudio/video/spring-boot-advanced', '2023-08-15'),
+                                                 ('Understanding SQL', 'https://rapharlstudio/video/sql-basics', '2023-07-20');
+INSERT INTO video (title, release_date) VALUES
+                                                 ('Understanding SQL', '2023-07-20');
