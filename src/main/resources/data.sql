3e55f09c09a87dc8d8cbae47f69de01bc0639b0f
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2024-09-02 08:35:26 +0200

    Alex, la in 3st music Object i databasen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,8 @@ INSERT INTO video (title, url, release_date) VALUES
                                                  ('Introduction to Java', 'https://rapharlstudio/video/java-intro', '2023-01-10'),
                                                  ('Advanced Spring Boot', 'https://rapharlstudio/video/spring-boot-advanced', '2023-08-15'),
                                                  ('Understanding SQL', 'https://rapharlstudio/video/sql-basics', '2023-07-20');
-INSERT INTO video (title, release_date) VALUES
-                                                 ('Understanding SQL', '2023-07-20');
+INSERT INTO music (title, url, release_date) VALUES
+                                                 ('Java Music', 'https://rapharlstudio/music/java-intro', '2020-05-13'),
+                                                 ('Advanced Music', 'https://rapharlstudio/music/spring-boot-advanced', '2019-10-01'),
+                                                 ('Music of SQL', 'https://rapharlstudio/music/sql-basics', '2022-01-30');
+
