617fedf46f86272d2129cc71351aefd7cdaf9ebe
msved <marcus.svedberg.java23@edu.edugrade.se>
2024-09-06 11:21:18 +0200

    Added sql file and customers to the database RG-31

---
+++ Customer-Service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (user_name, email_adress) VALUES ('Anna Andersson', 'anna.a@hotmail.com');
+INSERT INTO customer (user_name, email_adress) VALUES ('Karl Karlsson', 'kalle@gmail.com');
+INSERT INTO customer (user_name, email_adress) VALUES ('Per Persson', 'pelle.p@yahoo.se');
+INSERT INTO customer (user_name, email_adress) VALUES ('Erika Eriksson', 'e.e@spray.se');
+INSERT INTO customer (user_name, email_adress) VALUES ('Sven Svensson', 'svenne@outlook.com');
