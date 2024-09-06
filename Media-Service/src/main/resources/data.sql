6a2f1ce593c24a1b8b3d5680db603b4e73cb7de6
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2024-09-06 13:09:40 +0200

    sql media uppdaterad

--- Media-Service/src/main/resources/data.sql
+++ Media-Service/src/main/resources/data.sql
@@ -1,166 +1,159 @@
--- Lägg till artister
 INSERT INTO artist (artist_name)
-VALUES ('Queen'),
-       ('Michael Jackson'),
-       ('Dave Brubeck'),
-       ('James Cameron'),
-       ('Jeff Orlowski'),
-       ('David Attenborough'),
-       ('Freddie Mercury'),
-       ('Paul McCartney'),
-       ('John Lennon'),
-       ('Miles Davis Quintet'),
-       ('Hans Zimmer & Lisa Gerrard');
+VALUES
+    ('Leonardo DiCaprio'),
+    ('Quentin Tarantino'),
+    ('Hans Zimmer'),
+    ('David Attenborough'),
+    ('Emma Watson'),
+    ('Billie Eilish'),
+    ('Finneas O\Connell'),
+    ('Adele'),
+    ('Ed Sheeran'),
+    ('Beyoncé'),
+    ('Joe Rogan'),
+    ('Ricky Gervais'),
+    ('Dave Chappelle'),
+    ('Lex Fridman'),
+    ('Foo Fighters'),
+    ('Queen'),
+    ('The Rolling Stones'),
+    ('Malcolm Gladwell'),
+    ('Sarah Koenig'),
+    ('Tim Ferriss'),
+    ('Bruce Willis'),
+    ('Keanu Reeves');
 
--- Lägg till album
 INSERT INTO album (album_name, artist_id)
-VALUES ('A Night at the Opera', 1),
-       ('Thriller', 2),
-       ('Time Out', 3),
-       ('Abbey Road', 8),
-       ('Sketches of Spain', 9),
-       ('Gladiator Soundtrack', 10);
+VALUES
+    ('When We All Fall Asleep, Where Do We Go?', 6),
+    ('25', 8),
+    ('Divide', 9),
+    ('Lemonade', 10),
+    ('Happier Than Ever', 6),
+    ('Greatest Hits', 15),
+    ('A Night at the Opera', 16),
+    ('Sticky Fingers', 17);
 
--- Lägg till genrer
 INSERT INTO genre (genre_name)
-VALUES ('Drama'),
-       ('Documentary'),
-       ('Nature/Science'),
-       ('Rock'),
-       ('Pop'),
-       ('Jazz'),
-       ('History'),
-       ('True Crime'),
-       ('Science');
+VALUES
+    ('Drama'),
+    ('Action'),
+    ('Pop'),
+    ('Rock'),
+    ('Documentary'),
+    ('Comedy');
 
--- Lägg till all media
 INSERT INTO media (title, url, release_date, media_type)
-VALUES ('Titanic', 'https://raphaelstudio/video/titanic', '1997-12-19','video'),
-       ('The Social Dilemma', 'https://raphaelstudio/video/social-dilemma', '2020-01-26','video'),
-       ('Planet Earth', 'https://raphaelstudio/video/planet-earth', '2006-03-05','video'),
-       ('The Godfather', 'https://raphaelstudio/video/the-godfather', '1972-03-24','video'),
-       ('Forrest Gump', 'https://raphaelstudio/video/forrest-gump', '1994-07-06','video'),
-       ('Blackfish', 'https://raphaelstudio/video/blackfish', '2013-07-01','video'),
-       ('13th', 'https://raphaelstudio/video/13th', '2016-10-07','video'),
-       ('World War II Documentary', 'https://raphaelstudio/video/ww2-documentary', '2001-05-15','video'),
-       ('The Rise and Fall of the Roman Empire', 'https://raphaelstudio/video/roman-empire', '2018-09-10','video'),
-       ('Cosmos: A Spacetime Odyssey', 'https://raphaelstudio/video/cosmos', '2014-03-12','video'),
-       ('Bohemian Rhapsody - Queen', 'https://raphaelstudio/music/bohemian-rhapsody', '1975-10-31', 'music'),
-       ('Thriller - Michael Jackson', 'https://raphaelstudio/music/thriller', '1982-11-30', 'music'),
-       ('Take Five - Dave Brubeck', 'https://raphaelstudio/music/take-five', '1959-09-21', 'music'),
-       ('Come Together - The Beatles', 'https://raphaelstudio/music/come-together', '1969-09-26', 'music'),
-       ('Stairway to Heaven - Led Zeppelin', 'https://raphaelstudio/music/stairway-to-heaven', '1971-11-08', 'music'),
-       ('Like a Prayer - Madonna', 'https://raphaelstudio/music/like-a-prayer', '1989-03-03', 'music'),
-       ('Bad Romance - Lady Gaga', 'https://raphaelstudio/music/bad-romance', '2009-10-26', 'music'),
-       ('Take the A Train - Duke Ellington', 'https://raphaelstudio/music/take-the-a-train', '1941-09-15', 'music'),
-       ('So What - Miles Davis', 'https://raphaelstudio/music/so-what', '1959-08-17', 'music'),
-       ('The History of Ancient Rome', 'https://raphaelstudio/podcast/history-rome', '2024-01-01', 'podcast'),
-       ('True Crime Stories', 'https://raphaelstudio/podcast/true-crime-stories', '2019-03-10', 'podcast'),
-       ('The Science of Sleep', 'https://raphaelstudio/podcast/science-of-sleep', '2021-05-15', 'podcast'),
-       ('Making a Murderer', 'https://raphaelstudio/podcast/making-a-murderer', '2015-12-18', 'podcast'),
-       ('Serial', 'https://raphaelstudio/podcast/serial', '2014-10-03', 'podcast'),
-       ('The Elegant Universe', 'https://raphaelstudio/podcast/elegant-universe', '2003-01-01', 'podcast'),
-       ('Gladiator', 'https://raphaelstudio/video/gladiator', '2000-05-05', 'video'),
-       ('The Last Dance', 'https://raphaelstudio/video/the-last-dance', '2020-04-19', 'video'),
-       ('Blue Planet II', 'https://raphaelstudio/video/blue-planet-ii', '2017-10-29', 'video'),
-       ('Under Pressure - Queen & David Bowie', 'https://raphaelstudio/music/under-pressure', '1981-10-26', 'music'),
-       ('Billie Jean - Michael Jackson', 'https://raphaelstudio/music/billie-jean', '1983-01-02', 'music'),
-       ('Freddie Freeloader - Miles Davis', 'https://raphaelstudio/music/freddie-freeloader', '1959-03-02', 'music'),
-       ('Guns, Germs, and Steel', 'https://raphaelstudio/podcast/guns-germs-steel', '2005-07-10', 'podcast'),
-       ('The Jinx: The Life and Deaths of Robert Durst', 'https://raphaelstudio/podcast/the-jinx', '2015-02-08', 'podcast'),
-       ('Nova: Einstein’s Quantum Riddle', 'https://raphaelstudio/video/einsteins-quantum-riddle', '2019-01-09', 'video');
+VALUES
+    -- Drama
+    ('Inception', 'https://raphaelstudio/video/inception', '2010-07-16', 'video'),
+    ('Harry Potter and the Philosophers Stone', 'https://raphaelstudio/video//harry-potter', '2001-11-16', 'video'),
+    ('The Revenant', 'https://raphaelstudio/video/the-revenant', '2015-12-25', 'video'),
 
--- Lägg till videor med id
+    -- Action
+    ('Once Upon a Time in Hollywood', 'https://raphaelstudio/video/hollywood', '2019-07-26', 'video'),
+    ('Die Hard', 'https://raphaelstudio/video/die-hard', '1988-07-15', 'video'),
+    ('John Wick', 'https://raphaelstudio/video/john-wick', '2014-10-24', 'video'),
+
+    -- Pop
+    ('Bad Guy', 'https://raphaelstudio/music/bad-guy', '2019-03-29', 'music'),
+    ('Someone Like You', 'https://raphaelstudio/music/someone-like-you', '2011-01-24', 'music'),
+    ('Shape of You', 'https://raphaelstudio/music/shape-of-you', '2017-01-06', 'music'),
+
+    -- Rock
+    ('Everlong', 'https://raphaelstudio/music/everlong', '1997-08-18', 'music'),
+    ('Bohemian Rhapsody', 'https://raphaelstudio/music/bohemian-rhapsody', '1975-10-31', 'music'),
+    ('Brown Sugar', 'https://raphaelstudio/music/brown-sugar', '1971-04-16', 'music'),
+
+    -- Documentary
+    ('Planet Earth II', 'https://raphaelstudio/video/planet-earth-ii', '2016-11-06', 'video'),
+    ('The Joe Rogan Experience', 'https://raphaelstudio/podcast/jre', '2021-03-01', 'podcast'),
+    ('Serial', 'https://raphaelstudio/podcast/serial', '2014-10-03', 'podcast'),
+
+    -- Comedy
+    ('The Office', 'https://raphaelstudio/podcast/the-office', '2005-03-24', 'video'),
+    ('Ricky Gervais Show', 'https://raphaelstudio/podcast/ricky-gervais', '2005-11-25', 'podcast'),
+    ('The Tim Ferriss Show', 'https://raphaelstudio/podcast/tim-ferriss', '2016-07-05', 'podcast');
+
 INSERT INTO video(id)
-VALUES (1), (2), (3), (4), (5), (6), (7), (8), (9), (10), (26), (27), (28), (34);
+VALUES (1), (2), (3), (4), (5), (6), (13), (16);
 
--- Lägg till musik med id
 INSERT INTO music(id)
-VALUES (11), (12), (13), (14), (15), (16), (17), (18), (19), (29), (30), (31);
+VALUES (7), (8), (9), (10), (11), (12);
 
--- Lägg till podcasts med id
 INSERT INTO podcast(id)
-VALUES (20), (21), (22), (23), (24), (25), (32), (33);
+VALUES (14), (15), (17), (18);
 
--- Koppla genrer till media
 INSERT INTO media_genres (media_id, genre_id)
-VALUES (1, 1),
-       (2, 2),
-       (3, 3),
-       (4, 4),
-       (5, 5),
-       (6, 6),
-       (7, 4),
-       (8, 7),
-       (9, 8),
-       (10, 9),
-       (11, 1),
-       (12, 1),
-       (13, 2),
-       (14, 2),
-       (15, 4),
-       (16, 5),
-       (17, 5),
-       (18, 6),
-       (19, 6),
-       (20, 7),
-       (21, 7),
-       (22, 8),
-       (23, 8),
-       (24, 9),
-       (25, 9),
-       (26, 1),
-       (27, 2),
-       (28, 3),
-       (29, 4),
-       (30, 5),
-       (31, 6),
-       (32, 7),
-       (33, 8),
-       (34, 9);
+VALUES
+    -- Drama
+    (1, 1), -- Inception - Drama
+    (2, 1), -- Harry Potter - Drama
+    (3, 1), -- The Revenant - Drama
 
+    -- Action
+    (4, 2), -- Once Upon a Time in Hollywood - Action
+    (5, 2), -- Die Hard - Action
+    (6, 2), -- John Wick - Action
 
--- Koppla artister till media
+    -- Pop
+    (7, 3), -- Bad Guy - Pop
+    (8, 3), -- Someone Like You - Pop
+    (9, 3), -- Shape of You - Pop
+
+    -- Rock
+    (10, 4), -- Everlong - Rock
+    (11, 4), -- Bohemian Rhapsody - Rock
+    (12, 4), -- Brown Sugar - Rock
+
+    -- Documentary
+    (13, 5), -- Planet Earth II - Documentary
+    (14, 5), -- Joe Rogan - Documentary
+    (15, 5), -- Serial - Documentary
+
+    -- Comedy
+    (16, 6), -- The Office - Comedy
+    (17, 6), -- Ricky Gervais Show - Comedy
+    (18, 6); -- Tim Ferriss Show - Comedy
+
 INSERT INTO media_artists (media_id, artist_id)
 VALUES
-    -- Videor
-    (1, 4),
-    (2, 5),
-    (3, 6),
-    (4, 10),
-    (5, 5),
-    (6, 6),
-    (7, 10),
-    (26, 10),
-    (27, 5),
-    (28, 6),
-    (34, 10),
-    -- Musik
-    (11, 1),
-    (11, 7),
-    (12, 2),
-    (13, 3),
-    (14, 8),
-    (14, 9),
-    (15, 1),
-    (15, 7),
-    (16, 2),
-    (17, 9),
-    (29, 1),
-    (29, 7),
-    (30, 2),
-    (31, 9);
+    -- Drama
+    (1, 1), -- Inception - Leonardo DiCaprio
+    (2, 5), -- Harry Potter - Emma Watson
+    (3, 1), -- The Revenant - Leonardo DiCaprio
 
--- Koppla album till musik
+    -- Action
+    (4, 2), -- Once Upon a Time in Hollywood - Quentin Tarantino
+    (4, 1), -- Once Upon a Time in Hollywood - Leonardo Dicaprio
+    (5, 21), -- Die Hard - Hans Zimmer (kompositör)
+    (6, 22), -- John Wick - Hans Zimmer (kompositör)
+
+    -- Pop
+    (7, 6), -- Bad Guy - Billie Eilish
+    (8, 8), -- Someone Like You - Adele
+    (9, 9), -- Shape of You - Ed Sheeran
+
+    -- Rock
+    (10, 15), -- Everlong - Foo Fighters
+    (11, 16), -- Bohemian Rhapsody - Queen
+    (12, 17), -- Brown Sugar - The Rolling Stones
+
+    -- Documentary
+    (13, 4), -- Planet Earth II - David Attenborough
+    (14, 11), -- Joe Rogan - Joe Rogan
+    (15, 19), -- Serial - Sarah Koenig
+
+    -- Comedy
+    (16, 12), -- The Office - Ricky Gervais
+    (17, 12), -- Ricky Gervais Show - Ricky Gervais
+    (18, 20); -- Tim Ferriss Show - Tim Ferriss
+
 INSERT INTO media_albums (media_id, album_id)
 VALUES
-    (11, 1),
-    (12, 2),
-    (13, 3),
-    (14, 4),
-    (15, 1),
-    (16, 2),
-    (17, 3),
-    (29, 1),
-    (30, 2),
-    (31, 3);
+    (7, 1), -- Bad Guy - When We All Fall Asleep, Where Do We Go?
+    (8, 2), -- Someone Like You - 25
+    (9, 3), -- Shape of You - Divide
+    (10, 6), -- Everlong - Greatest Hits (Foo Fighters)
+    (11, 7), -- Bohemian Rhapsody - A Night at the Opera (Queen)
+    (12, 8); -- Brown Sugar - Sticky Fingers (The Rolling Stones)
