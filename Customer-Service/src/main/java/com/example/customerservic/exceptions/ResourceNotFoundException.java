97b05d8da55a952e868db46ed344c73c75838839
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2024-09-09 08:55:16 +0200

    Alex. Upprätta endpoint för att ta bort en customer. RG51

---
+++ Customer-Service/src/main/java/com/example/customerservic/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,31 @@
+package com.example.customerservic.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String name;
+    private String field;
+    private Object value;
+
+    public ResourceNotFoundException(String name, String field, Object value) {
+        super(String.format("%s with %s '%s' was not found", name, field, value));
+        this.name = name;
+        this.field = field;
+        this.value = value;
+    }
+
+    public String getItem() {
+        return name;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+}
