aaea74b716668d99f6f4ffd71e6d142912953ddf
msved <marcus.svedberg.java23@edu.edugrade.se>
2024-09-12 15:44:59 +0200

    Created endpoint to set like/dislike by customer RG-15

--- Edufy-Service/src/main/java/com/example/edufy/controllers/EdufyUserController.java
+++ Edufy-Service/src/main/java/com/example/edufy/controllers/EdufyUserController.java
@@ -1,9 +1,6 @@
 package com.example.edufy.controllers;
 
-import com.example.edufy.VO.Customer;
-import com.example.edufy.VO.Media;
-import com.example.edufy.VO.MediaInteractions;
-import com.example.edufy.VO.MediaRequstBody;
+import com.example.edufy.VO.*;
 import com.example.edufy.services.EdufyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -48,6 +45,14 @@ public class EdufyUserController {
     public ResponseEntity<String> playMediaAndIncreaseTimesListenedTo(@RequestBody MediaRequstBody mediaRequstBody){
         Media playMedia = edufyUserService.playAndUpdateListedToInCustomer(mediaRequstBody.getCustomerId(), mediaRequstBody.getMediaId());
         return new ResponseEntity<>("This " + playMedia.getMediaType() + " is playing \n" + playMedia.getURL(), HttpStatus.OK);
+
+    }
+
+    @PostMapping("/ratemedia")
+    public ResponseEntity<String> rateMedia(@RequestBody CustomerMediaRequestBody requestBody){
+        MediaInteractions result = edufyUserService.rateMedia(requestBody.getCustomerId(), requestBody.getMediaId(), requestBody.getLikeStatus());
+        String message = "Customer with id: " + requestBody.getCustomerId() + " set " + requestBody.getLikeStatus() + " on media with id: " + requestBody.getMediaId();
+        return ResponseEntity.ok(message);
 
     }
 }
