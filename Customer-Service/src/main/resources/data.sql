abfb678cf2b5396b533392247b0125c96957c080
Johanna Selander <johanna.selander.JAVA23@edu.edugrade.se>
2024-09-13 14:30:57 +0200

    getRecommendedMedia funkar med lämplig logik

--- Customer-Service/src/main/resources/data.sql
+++ Customer-Service/src/main/resources/data.sql
@@ -12,8 +12,13 @@ VALUES ('Sven Svensson', 'svenne@outlook.com');
 
 INSERT INTO media_interactions (media_id, like_status, times_listened_to, customer_id)
 VALUES (1, 'like', 4, 1),
-       (2, '', 2, 1),
-       (3, 'dislike', 7, 1),
+       (4, '', 2, 1),
+       (7, 'dislike', 7, 1),
+       (10, 'like', 5, 1),
+       (14, 'like', 10, 1),
+       (16, '', 8, 1),
+       (11, '', 3, 1),
+       (9, '', 1, 1),
        (1, 'like', 20, 2),
        (2, 'dislike', 1, 2),
        (4, '', 3, 2),
